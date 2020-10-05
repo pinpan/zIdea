c7f460cef0f959d274f4d23d23b2b988f4e3b4a4
GitHub Action <action@github.com>
2020-10-05 10:57:19 +0000

    Template cleanup

--- src/main/kotlin/com/github/pinpan/zowedeal/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/pinpan/zowedeal/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.pinpan.zowedeal.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pinpan.zowedeal.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/pinpan/zowedeal/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pinpan/zowedeal/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.pinpan.zowedeal.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pinpan.zowedeal.MyBundle
 
 class MyProjectService(project: Project) {
 
